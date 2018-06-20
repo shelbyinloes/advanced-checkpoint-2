08db794aa02db3161381728125d111f3389a4eb9
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-20 18:26:16 -0500

    bootstrap added:

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import {Button} from "react-bootstrap";
 
 class App extends Component {
   constructor() {
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <div>
         Client
+        <Button />
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import AppContainer from "./AppContainer";
 import "./index.css";
 import store from "./store";
 import {Provider} from "react-redux";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
 
 ReactDOM.render(
   <Provider store={store}><AppContainer /></Provider>,
